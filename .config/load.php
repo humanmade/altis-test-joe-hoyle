313bbe74355e06214d0350509610472dd7a0dbaa
Joe Hoyle <joehoyle@gmail.com>
2023-02-15 17:44:57 +0200

    Update auth

--- .config/load.php
+++ .config/load.php
@@ -1,3 +1,3 @@
 <?php
 
-ini_set( 'afterburner.redis_server_info', sprintf('redis://%s:%d', getenv( 'REDIS_HOST' ), getenv( 'REDIS_PORT' ) ) );
+ini_set( 'afterburner.redis_server_info', sprintf('rediss://:%s@%s:%d', getenv( 'REDIS_AUTH' ), getenv( 'REDIS_HOST' ), getenv( 'REDIS_PORT' ) ) );
