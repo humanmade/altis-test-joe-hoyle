bbead672c685a847f09b668064ae230992e5fe54
Joe Hoyle <joehoyle@gmail.com>
2023-02-15 19:08:30 +0200

    Skil server check

--- .config/load.php
+++ .config/load.php
@@ -1,3 +1,4 @@
 <?php
 
 ini_set( 'afterburner.redis_server_info', sprintf('rediss://:%s@%s:%d', getenv( 'REDIS_AUTH' ), getenv( 'REDIS_HOST' ), getenv( 'REDIS_PORT' ) ) );
+ini_set( 'afterburner.redis_skip_server_check', 'yes' );
